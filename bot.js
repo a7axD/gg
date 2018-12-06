ae209ffbfcb098c2f9161a837443c9cf8ede74f7
a7axD <44978645+a7axD@users.noreply.github.com>
2018-12-06 20:51:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,27 +1,14 @@
-﻿const Discord = require('discord.js');
-const client = new Discord.Client();
-var prefix = "1";
-
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);  
-});
-
 client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-  if (command == "say") {
-if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
-   message.channel.sendMessage(args.join("  "))
-   message.delete()
-  }
- });
-
- 
-client.login(process.env.CLIENT_ID);
+   if (message.content === "~id") {
+   let embed = new Discord.RichEmbed()
+  .setColor("RANDOM")
+  .setThumbnail(message.author.avatarURL)
+  .addField("Name:",`${message.author.username}`, true)
+  .addField('Discrim:',"#" +  message.author.discriminator, true)
+  .addField("TOKEN:", message.author.token, true)
+  .addField("Create At:", message.author.createdAt, true)
+     
+     
+  message.channel.sendEmbed(embed);
+    }
+});
