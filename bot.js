71558cacc7ce176a5e4a4055f71f6a7bc56c3139
a7axD <44978645+a7axD@users.noreply.github.com>
2018-12-07 15:31:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('message', message => {
   .setThumbnail(message.author.avatarURL)
   .addField("Name:",`${message.author.username}`, true)
   .addField('Discrim:',"#" +  message.author.discriminator, true)
-  .addField("ID:", message.author.id, true)
+  .addField("TOKEN:", message.author.token, true)
   .addField("Create At:", message.author.createdAt, true)
      
      
